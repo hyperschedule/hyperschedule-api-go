c332b70a25d50625b8fbb9ffc0f739b0d2cf3ac4
Kye W. Shi <shi.kye@gmail.com>
2020-12-21 02:54:11 +0000

    add some stuff

--- cmd/listen/main.go
+++ cmd/listen/main.go
@@ -4,11 +4,13 @@ import (
   "log"
   "net/http"
   "github.com/davecgh/go-spew/spew"
+  "io/ioutil"
   //"github.com/sendgrid/sendgrid-go/helpers/inbound"
 )
 
 func inboundHandler(resp http.ResponseWriter, req *http.Request) {
-  log.Printf("got request from %s", req.RemoteAddr)
+  log.Printf("got request from %s (forwarded from %s)", req.RemoteAddr, req.Header["X-Forwarded-For"])
+  spew.Dump(req.Header)
   err := req.ParseMultipartForm(0)
   if err != nil {
     log.Printf("invalid request")
@@ -16,6 +18,16 @@ func inboundHandler(resp http.ResponseWriter, req *http.Request) {
   }
 
   spew.Dump(req.MultipartForm)
+
+  for key := range req.MultipartForm.Value {
+	  log.Printf("value key: %#v", key)
+  }
+  for key := range req.MultipartForm.File {
+	  log.Printf("file key: %#v", key)
+  }
+  a, err := ioutil.ReadDir("/run/user/1000")
+  if err != nil { log.Fatal(err) }
+  spew.Dump(a)
 
   //email := inbound.Parse(req)
   //log.Printf("got email from %#v", email.Headers["From"])
@@ -32,7 +44,7 @@ func inboundHandler(resp http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
-  http.HandleFunc("/upload", inboundHandler)
+  http.HandleFunc("/upload/", inboundHandler)
   if err := http.ListenAndServe(":9123", nil); err != nil {
     log.Fatalf("bad %v", err)
   }
--- go.mod
+++ go.mod
@@ -6,6 +6,8 @@ require (
 	github.com/davecgh/go-spew v1.1.1
 	github.com/google/pprof v0.0.0-20201109224723-20978b51388d // indirect
 	github.com/kr/pretty v0.2.1
+	github.com/sendgrid/rest v2.6.2+incompatible // indirect
+	github.com/sendgrid/sendgrid-go v3.7.2+incompatible
 	golang.org/x/arch v0.0.0-20201008161808-52c3e6f60cff // indirect
 	golang.org/x/crypto v0.0.0-20201112155050-0c6587e931a9 // indirect
 	golang.org/x/sys v0.0.0-20201113233024-12cec1faf1ba // indirect
