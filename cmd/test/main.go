f503dd0f472f70509bc63a4a2a65109658744296
Kye Shi <shi.kye@gmail.com>
2020-12-22 14:16:52 -0800

    remove old fakecsv parser attempt

--- cmd/test/main.go
+++ cmd/test/main.go
@@ -1,16 +1,48 @@
 package main
 
 import (
+  //"io"
   "log"
-  "bufio"
+  "io/ioutil"
   "os"
-  "github.com/MuddCreates/hyperschedule-api-go/internal/fakecsv"
+  "github.com/MuddCreates/hyperschedule-api-go/internal/badcsv"
+  //"github.com/kr/pretty"
+  "github.com/davecgh/go-spew/spew"
 )
 
 func main() {
-	r := fakecsv.New(bufio.NewReader(os.Stdin))
-	row, err := r.ReadRow()
-	if err != nil { log.Fatalf("aaa %v", err) }
-	log.Printf("%#v\n", row)
-	//log.Println(parseFakeCsv(r))
+  f, err := os.Open("sample/course_1.csv")
+  if err != nil {
+    log.Fatalf("couldn't open file: %v", err)
+  }
+
+  input, err := ioutil.ReadAll(f)
+  if err != nil {
+    log.Fatalf("failed to read: %v", err)
+  }
+  _, warns, fails, err := badcsv.Parse(input)
+  if err != nil {
+    log.Fatalf("fail: %v", err)
+  }
+  //pretty.Logln(x)
+  //log.Println("nice")
+  spew.Dump(warns)
+  spew.Dump(fails)
+
+
+  //_ = f
+
+  /*
+	r := fakecsv.New(bufio.NewReader(f))
+  for {
+	  _, err := r.ReadRow()
+    if err == io.EOF {
+      break
+    }
+	  if err != nil {
+      log.Fatalf("failed to read csv row: %v", err)
+    }
+  }
+  log.Printf("successfully read all rows")
+  */
 }
