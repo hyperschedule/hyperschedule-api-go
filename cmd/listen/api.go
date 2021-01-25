c25db0c69f48d0a96bdffa336209d13cd1e5959a
Kye Shi <shi.kye@gmail.com>
2021-01-25 02:14:56 -0800

    add safety-check to avoid panic

--- cmd/listen/api.go
+++ cmd/listen/api.go
@@ -8,10 +8,17 @@ import (
 )
 
 func apiV3Handler(resp http.ResponseWriter, req *http.Request) {
+  data := state.GetData()
+  if data == nil {
+    log.Printf("received api request before loaded")
+    resp.WriteHeader(http.StatusServiceUnavailable)
+    return
+  }
   output, err := json.Marshal(api.MakeV3(state.GetData()))
   if err != nil {
     log.Printf("api: failed to jsonify, %s", err)
     resp.WriteHeader(http.StatusInternalServerError)
+    return
   }
 
   resp.Header().Add("Content-Type", "application/json")
