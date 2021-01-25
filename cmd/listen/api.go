ddc5b421724fb1d3cd9ac9a2606397e66b71103e
Kye Shi <shi.kye@gmail.com>
2021-01-25 02:06:09 -0800

    Use v3 api output instead of raw

--- cmd/listen/api.go
+++ cmd/listen/api.go
@@ -1,17 +1,19 @@
 package main
 
 import (
+  "github.com/MuddCreates/hyperschedule-api-go/internal/api"
   "log"
   "net/http"
   "encoding/json"
 )
 
 func apiV3Handler(resp http.ResponseWriter, req *http.Request) {
-  output, err := json.Marshal(state.GetData())
+  output, err := json.Marshal(api.MakeV3(state.GetData()))
   if err != nil {
-    log.Printf("api: failed, %s", err)
+    log.Printf("api: failed to jsonify, %s", err)
     resp.WriteHeader(http.StatusInternalServerError)
   }
+
   resp.Header().Add("Content-Type", "application/json")
   resp.Write(output)
 }
