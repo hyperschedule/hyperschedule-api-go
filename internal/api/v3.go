e3b382fc284112dc17454c7318aed9a6cf07bc9c
Kye Shi <shi.kye@gmail.com>
2021-04-05 11:27:37 -0700

    hotfix: FA2021

--- internal/api/v3.go
+++ internal/api/v3.go
@@ -1,21 +1,21 @@
 package api
 
 import (
-  "github.com/MuddCreates/hyperschedule-api-go/internal/data"
-  "fmt"
-  "strings"
-  "time"
-  "regexp"
-  "log"
+	"fmt"
+	"github.com/MuddCreates/hyperschedule-api-go/internal/data"
+	"log"
+	"regexp"
+	"strings"
+	"time"
 )
 
 var reCourseCode = regexp.MustCompile(fmt.Sprintf(
-  `^%s *%s *%s-%s %s$`,
-  `([A-Z]+)`,     // department id
-  `([0-9]+)`,     // course number
-  `([A-Z]+)`,     // college id
-  `(\d{2})`,      // section id
-  `([A-Z0-9]+)`,  // term code
+	`^%s *%s *%s-%s %s$`,
+	`([A-Z]+)`,    // department id
+	`([0-9]+)`,    // course number
+	`([A-Z]+)`,    // college id
+	`(\d{2})`,     // section id
+	`([A-Z0-9]+)`, // term code
 ))
 
 type V3 struct {
@@ -31,9 +31,9 @@ type V3CourseData struct {
 }
 
 type V3Term struct {
-	Code     string        `json:"termCode"`
-	SortKey  []interface{} `json:"termSortKey"`
-	Name string        `json:"termName"`
+	Code    string        `json:"termCode"`
+	SortKey []interface{} `json:"termSortKey"`
+	Name    string        `json:"termName"`
 }
 
 type V3Course struct {
@@ -41,15 +41,15 @@ type V3Course struct {
 	Name               string        `json:"courseName"`
 	SortKey            []interface{} `json:"courseSortKey"`
 	MutualExclusionKey []interface{} `json:"courseMutualExclusionKey"`
-	Description        string       `json:"courseDescription"`
+	Description        string        `json:"courseDescription"`
 	Instructors        []string      `json:"courseInstructors"`
 	Term               string        `json:"courseTerm"`
 	Schedule           []*V3Schedule `json:"courseSchedule"`
 	Credits            float32       `json:"courseCredits"`
-	SeatsTotal         int          `json:"courseSeatsTotal"`
-	SeatsFilled        int          `json:"courseSeatsFilled"`
+	SeatsTotal         int           `json:"courseSeatsTotal"`
+	SeatsFilled        int           `json:"courseSeatsFilled"`
 	WaitlistLength     *int          `json:"courseWaitlistLength"`
-	EnrollmentStatus   string       `json:"courseEnrollmentStatus"`
+	EnrollmentStatus   string        `json:"courseEnrollmentStatus"`
 }
 
 type V3Schedule struct {
@@ -64,96 +64,96 @@ type V3Schedule struct {
 }
 
 func MakeV3(d *data.Data) *V3 {
-  terms := make(map[string]*V3Term)
-  for id, term := range d.Terms {
-    terms[id] = TermToV3Term(id, term)
-  }
+	terms := make(map[string]*V3Term)
+	for id, term := range d.Terms {
+		terms[id] = TermToV3Term(id, term)
+	}
 
-  return &V3{
-    Data: &V3CourseData{
-      Terms: terms,
-      Courses: MakeV3Courses(d),
-    },
-    Until: time.Now().Unix(),
-    Error: nil,
-    Full: true,
-  }
+	return &V3{
+		Data: &V3CourseData{
+			Terms:   terms,
+			Courses: MakeV3Courses(d),
+		},
+		Until: time.Now().Unix(),
+		Error: nil,
+		Full:  true,
+	}
 }
 
 func TermToV3Term(id string, t *data.Term) *V3Term {
-  return &V3Term{
-    Code: id,
-    Name: t.Semester,
-    SortKey: []interface{}{id, t.Semester},
-  }
+	return &V3Term{
+		Code:    id,
+		Name:    t.Semester,
+		SortKey: []interface{}{id, t.Semester},
+	}
 }
 
 func MakeV3Courses(d *data.Data) map[string]*V3Course {
-  courses := make(map[string]*V3Course)
-  for id, cs := range d.CourseSections {
-    course := d.Courses[cs.Course]
-    term := d.Terms[cs.Term]
+	courses := make(map[string]*V3Course)
+	for id, cs := range d.CourseSections {
+		course := d.Courses[cs.Course]
+		term := d.Terms[cs.Term]
 
-    // more dirty hacks
-    if !strings.HasPrefix(cs.Term, "SP2021") {
-      continue
-    }
+		// more dirty hacks
+		if !strings.HasPrefix(cs.Term, "FA2021") {
+			continue
+		}
 
-    matches := reCourseCode.FindStringSubmatch(id)
-    if matches == nil {
-      log.Printf("invalid course id: %s", id)
-      continue
-    }
-	  code := fmt.Sprintf("%s %s %s-%s", matches[1], matches[2], matches[3], matches[4])
+		matches := reCourseCode.FindStringSubmatch(id)
+		if matches == nil {
+			log.Printf("invalid course id: %s", id)
+			continue
+		}
+		code := fmt.Sprintf("%s %s %s-%s", matches[1], matches[2], matches[3], matches[4])
 
-    instructors := make([]string, 0)
-    for _, staff := range cs.Staff {
-      name := d.Staff[staff]
-      instructors = append(instructors, fmt.Sprintf("%s %s", name.First, name.Last))
-    }
+		instructors := make([]string, 0)
+		for _, staff := range cs.Staff {
+			name := d.Staff[staff]
+			instructors = append(instructors, fmt.Sprintf("%s %s", name.First, name.Last))
+		}
 
-    // these are dirty hacks, fix
-    termCount := 1
-    if cs.Term != term.Semester {
-      termCount = 2
-    }
+		// these are dirty hacks, fix
+		termCount := 1
+		if cs.Term != term.Semester {
+			termCount = 2
+		}
 
-    termIndex := 0
-    if termCount == 2 && strings.HasSuffix(cs.Term, "2") {
-      termIndex = 1
-    }
+		termIndex := 0
+		if termCount == 2 && strings.HasSuffix(cs.Term, "2") {
+			termIndex = 1
+		}
 
-    schedule := make([]*V3Schedule, 0)
-    for _, sched := range cs.Schedule {
+		schedule := make([]*V3Schedule, 0)
+		for _, sched := range cs.Schedule {
 
-      schedule = append(schedule, &V3Schedule{
-        Days: sched.Days.String(),
-        StartTime: sched.Start.String(),
-        EndTime: sched.End.String(),
-        StartDate: term.Start.String(),
-        EndDate: term.End.String(),
-        TermCount: termCount,
-        Terms: []int{termIndex},
-        Location: sched.Location,
-      })
-    }
+			schedule = append(schedule, &V3Schedule{
+				Days:      sched.Days.String(),
+				StartTime: sched.Start.String(),
+				EndTime:   sched.End.String(),
+				StartDate: term.Start.String(),
+				EndDate:   term.End.String(),
+				TermCount: termCount,
+				Terms:     []int{termIndex},
+				Location:  sched.Location,
+			})
+		}
 
-    courses[id] = &V3Course{
-      Code: code,
-      Name: course.Title,
-      SortKey: []interface{}{id},
-      MutualExclusionKey: []interface{}{cs.Course},
-      Description: course.Description,
-      Instructors: instructors,
-      Term: cs.Term,
-      Schedule: schedule,
-      Credits: float32(cs.QuarterCredits)/4,
-      SeatsTotal: cs.Seats.Capacity,
-      SeatsFilled: cs.Seats.Enrolled,
-      WaitlistLength: nil,
-      EnrollmentStatus: cs.Status.String(),
-    }
-  }
+		courses[id] = &V3Course{
+			Code:               code,
+			Name:               course.Title,
+			SortKey:            []interface{}{id},
+			MutualExclusionKey: []interface{}{cs.Course},
+			Description:        course.Description,
+			Instructors:        instructors,
+			Term:               cs.Term,
+			Schedule:           schedule,
+			Credits:            float32(cs.QuarterCredits) / 4,
+			SeatsTotal:         cs.Seats.Capacity,
+			SeatsFilled:        cs.Seats.Enrolled,
+			WaitlistLength:     nil,
+			EnrollmentStatus:   cs.Status.String(),
+		}
+	}
 
-  return courses
+	return courses
 }
