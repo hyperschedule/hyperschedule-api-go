1518c4caabc6914dafabb5f1fe82b7c023e2312b
Kye Shi <shi.kye@gmail.com>
2021-04-10 17:20:40 -0700

    add release cmd

---
+++ cmd/release/release.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	"log"
+	"os"
+)
+
+func main() {
+	dbUrl := os.Getenv("DATABASE_URL")
+	if len(dbUrl) == 0 {
+		log.Fatalf("DATABASE_URL undefined")
+	}
+
+	m, err := migrate.New("file://migrate", dbUrl)
+	if err != nil {
+		log.Fatalf("failed to init migrate: %v", err)
+	}
+
+	if err := m.Up(); err != nil {
+		if err == migrate.ErrNoChange {
+			log.Printf("no migrations performed (up to date)")
+		} else {
+			log.Fatalf("failed to up migrate: %v", err)
+		}
+	}
+}
--- tools.go
+++
@@ -1,8 +0,0 @@
-// +build tools
-
-package tools
-
-import (
-	_ "github.com/golang-migrate/migrate/v4"
-	_ "github.com/golang-migrate/migrate/v4/cmd/migrate"
-)
