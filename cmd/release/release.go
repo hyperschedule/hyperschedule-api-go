b989708f36faed19e33ad3819dd8a44b60a8c70b
Kye Shi <shi.kye@gmail.com>
2021-04-10 17:22:36 -0700

    import file migrate source

--- cmd/release/release.go
+++ cmd/release/release.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"log"
 	"os"
 )
