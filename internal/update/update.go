68fd4539189105696bcb1a9a90ab2f8200def954
Kye Shi <shi.kye@gmail.com>
2021-08-26 12:21:33 -0700

    log progress during update

--- internal/update/update.go
+++ internal/update/update.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/MuddCreates/hyperschedule-api-go/internal/data"
 	"github.com/MuddCreates/hyperschedule-api-go/internal/db"
+	"log"
 )
 
 type Summary struct {
@@ -13,30 +14,36 @@ type Summary struct {
 }
 
 func Update(ctx context.Context, pool *db.Connection, d *data.Data) (Summary, error) {
+	log.Printf("initiating transaction")
 	tx, err := pool.Begin(ctx)
 	if err != nil {
 		return Summary{}, fmt.Errorf("failed to begin transaction: %w", err)
 	}
 	defer tx.Cleanup()
 
+	log.Printf("creating temporary tables")
 	if err := tmpTables.create(tx); err != nil {
 		return Summary{}, fmt.Errorf("failed to create temporary tables: %w", err)
 	}
 
+	log.Printf("populating temporary tables")
 	summaryCopy, err := tmpTables.copyFrom(tx, rowsFrom(d))
 	if err != nil {
 		return Summary{}, fmt.Errorf("failed to populate temporary tables: %w", err)
 	}
 
+	log.Printf("applying modifications to persistent tables")
 	summaryApply, err := tmpTables.apply(tx)
 	if err != nil {
 		return Summary{}, fmt.Errorf("failed to apply temporary table changes: %w", err)
 	}
 
+	log.Printf("committing changes")
 	if err := tx.Commit(); err != nil {
 		return Summary{}, fmt.Errorf("failed to commit transaction: %w", err)
 	}
 
+	log.Printf("done")
 	return Summary{
 		Copy:  summaryCopy,
 		Apply: summaryApply,
